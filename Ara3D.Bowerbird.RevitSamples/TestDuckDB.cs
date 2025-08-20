0571d320736e3c04a8277ce17d7dbc8327f145b7
Christopher Diggins <cdiggins@gmail.com>
2025-08-19 20:50:56 -0400

    Adding TestDucDB

---
+++ Ara3D.Bowerbird.RevitSamples/TestDuckDB.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Ara3D.SpaceLinter.RevitSamples;
+
+public class TestDuckDB : NamedCommand
+{
+    public override string Name => "Test DuckDB";
+
+    public override void Execute(object arg)
+    {
+        var s1 = Assembly.GetExecutingAssembly().Location;
+        var s2 = Environment.CurrentDirectory;
+        MessageBox.Show(s1 + "\n" + s2);
+
+        NativeLibrary.Load(@"C:\Users\cdigg\AppData\Roaming\Autodesk\Revit\Addins\2025\Ara3D.BIMOpenSchema\duckdb.dll");
+        NativeLibrary.Load(@"C:\Users\cdigg\AppData\Roaming\Autodesk\Revit\Addins\2025\Ara3D.BIMOpenSchema\VCRUNTIME140.dll");
+        NativeLibrary.Load(@"C:\Users\cdigg\AppData\Roaming\Autodesk\Revit\Addins\2025\Ara3D.BIMOpenSchema\nironcompress.dll");
+
+        var sb = new DuckDB.NET.Data.DuckDBConnectionStringBuilder();
+        sb.DataSource = "File";
+    }
+}
